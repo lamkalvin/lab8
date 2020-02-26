c86a7c7871028c3abc50872eb2f7a2059b2c4092
Kalvin Lam <kkl038@ucsd.edu>
2020-02-26 15:28:18 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = '8aPEJOjL30bA8t78K44b1lqrPCSqBH71';
+
+    // 'map' refers to a <div> element with the ID map
+    var map = L.mapquest.map('map', {
+        center: [32.88, -117.236],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 12,
+        zoomControl: false
+    });
+
+    L.marker([32.88, -117.236]).addTo(map);
 }
